2d9369527267646db0ca485ffa7f1706bfa6d29c
trung-tin-dev <tatin3469@gmail.com>
2026-05-06 11:49:12 +0700

    getUserID-By-JWT

--- src/ZipURL.Services.ShorterURL/Features/ShortenURLFeature/CreateShortCode/CreateShortCodeRequest.cs
+++ src/ZipURL.Services.ShorterURL/Features/ShortenURLFeature/CreateShortCode/CreateShortCodeRequest.cs
@@ -1,4 +1,4 @@
 ﻿namespace ZipURL.Services.ShorterURL.Features.ShortenURLFeature.CreateShortCode
 {
-    public record CreateShortCodeRequest(string TargetUrl, string UserId);
+    public record CreateShortCodeRequest(string TargetUrl);
 }
